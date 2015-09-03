71915aa3c22dbd5a7bd3937b472b25e84004e92c
Il Gu Yi <gubs@Il-Gu-Yiui-MacBook-Pro-Retina.local>
2015-09-03 18:53:14 +0900

    parameters.d

---
+++ parameters.d
@@ -0,0 +1,29 @@
+// Reading Parameters setting file
+// DO NOT ERASE ALL ANNOTATION
+// Number of training data
+250
+// Number of input data dimension
+784
+// Number of validation data
+50
+// Number of test data
+30
+// Number of class
+10
+// Number of layers
+4
+// Learning rate
+0.005
+// Cost function type (CrossEntropy or Quadratic)
+// using quadratic cost function C = (target - activation)^2 / 2
+// using cross entropy cost function
+// C = target * log activation + (1 - target) * log (1 - activation) 
+CrossEntropy
+// Regularization parameter
+0.00001
+// Momentum parameter
+0.95
+// Mini batch size
+10
+// iteration max epochs
+30
